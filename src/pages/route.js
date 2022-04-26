1d2575e227e80285985f4c19f0d5fa0ec817e7bb
ToeWai27 <toewai@nexbe.sg>
2022-04-26 10:55:05 +0630

    FeedsApp

---
+++ src/pages/route.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Article from "./Article";
+import Authentication from "./authentication";
+import GlobalFeed from "./Globalfeed";
+
+export default function Routess() {
+  return (
+    <Routes>
+      <Route path="/" element={<GlobalFeed />} exact="true" />
+      <Route path="/articles/:slug" element={<Article />} />
+      <Route path="/login" element={<Authentication />} />
+    </Routes>
+  );
+}
